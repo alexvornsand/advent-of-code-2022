ae1bb74241ade6a9ad6daaca0d0cf24fdca60d3d
alexvornsand <ajvornsand@ucdavis.edu>
2022-12-01 09:39:55 -0800

    Initial commit

---
+++ solutions/day-01/day-01.py
@@ -0,0 +1,17 @@
+# advent of code
+# day 1
+
+# part 1
+foods = [[int(f) for f in b.split('\n')] for b in open('input.txt', 'r').read()[:-1].split('\n\n')]
+
+def mostFoods(foods, partTwo=False):
+    if partTwo is True:
+        n = 3
+    else:
+        n = 1
+    return(sum(sorted([sum(food) for food in foods], reverse=True)[:n]))
+
+mostFoods(foods)
+
+# part 2
+mostFoods(foods, partTwo=True)
