807ed3b6609728f6ef6777d94943c89b456a9069
alexvornsand <ajvornsand@ucdavis.edu>
2022-12-02 12:01:50 -0800

    solve day 2

---
+++ solutions/day-02/day-02.py
@@ -0,0 +1,24 @@
+# advent of code
+# day 2
+
+# part 1
+rounds = [r.split(' ') for r in open('input.txt', 'r').read().split('\n')[:-1]]
+
+def scoreRounds(rounds, partTwo=False):
+    intMap = {
+        'A':1,
+        'B':2,
+        'C':3,
+        'X':1,
+        'Y':2,
+        'Z':3
+    }
+    if partTwo is False:
+        return(sum([intMap[round[1]] + 3 * ((intMap[round[1]] - intMap[round[0]] + 1) % 3) for round in rounds]))
+    else: 
+        return(sum([(3 * intMap[round[1]]) + ((intMap[round[0]] + intMap[round[1]]) % 3) - 2 for round in rounds]))
+
+scoreRounds(rounds)
+
+# part 2
+scoreRounds(rounds, True)
