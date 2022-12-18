f4a6347a1104789ef4c0a29a335c05de1f6d3f94
alexvornsand <ajvornsand@ucdavis.edu>
2022-12-17 19:17:49 -0800

    Update day-16.py

--- solutions/day-16/day-16.py
+++ solutions/day-16/day-16.py
@@ -61,74 +61,24 @@ def releasePressure(valveRates, start=['AA'], timeRemaining=30, openedValves=[],
         return(pathBetweenValves)
     def findPath(valves, pathBetweenValves, valveSequence, unopenedValves, openedValves, timeRemaining, pressureSoFar):
         valve = valveSequence[-1]
-        if timeRemaining == 0:
-            return(valveSequence, pressureSoFar)
-        elif valve in unopenedValves and valves[valve]['rate'] > 0:
-            nextValveSequence = valveSequence.copy()
-            nextPressureSoFar = pressureSoFar + sum([valves[open]['rate'] for open in openedValves])
-            nextOpenedValves = openedValves.copy()
-            nextOpenedValves += [valve]
-            nextUnopenedValves = unopenedValves.copy()
-            nextUnopenedValves.remove(valve)
-            nextTimeRemaining = timeRemaining - 1
-            return(findPath(
-                valves=valves, 
-                pathBetweenValves=pathBetweenValves, 
-                valveSequence=nextValveSequence, 
-                unopenedValves=nextUnopenedValves, 
-                openedValves=nextOpenedValves, 
-                timeRemaining=nextTimeRemaining, 
-                pressureSoFar=nextPressureSoFar
-            ))
-        elif len(unopenedValves) == 0:
-            nextValveSequence = valveSequence.copy()
-            nextPressureSoFar = pressureSoFar + sum([valves[open]['rate'] for open in openedValves])
-            nextOpenedValves = openedValves.copy()
-            nextUnopenedValves = unopenedValves.copy()
-            nextTimeRemaining = timeRemaining - 1
-            return(findPath(
-                valves=valves, 
-                pathBetweenValves=pathBetweenValves, 
-                valveSequence=nextValveSequence, 
-                unopenedValves=nextUnopenedValves, 
-                openedValves=nextOpenedValves, 
-                timeRemaining=nextTimeRemaining, 
-                pressureSoFar=nextPressureSoFar
-            ))
+        potentialNextValves = [nextValve for nextValve in unopenedValves if len(pathBetweenValves[valve, nextValve]) < timeRemaining - 2]
+        if timeRemaining == 0 or len(potentialNextValves) == 0:
+            nextPressureSoFar = pressureSoFar + sum([valves[open]['rate'] * timeRemaining for open in openedValves])
+            return(valveSequence, nextPressureSoFar)
         else:
-            potentialNextValves = [nextValve for nextValve in unopenedValves if len(pathBetweenValves[valve, nextValve]) < timeRemaining - 2]
-            if len(potentialNextValves) > 0:
-                nextSteps = []
-                for nextValve in potentialNextValves:
-                    nextValveSequence = valveSequence.copy()
-                    nextValveSequence += [nextValve]
-                    nextPressureSoFar = pressureSoFar + sum([valves[openValve]['rate'] * len(pathBetweenValves[(valve, nextValve)]) for openValve in openedValves])
-                    nextOpenedValves = openedValves.copy()
-                    nextUnopenedValves = unopenedValves.copy()
-                    nextTimeRemaining = timeRemaining - len(pathBetweenValves[valve, nextValve])
-                    if valveSequence == originalInput:
-                        print('valveRates=valveRates, start=' + str(nextValveSequence) + ', timeRemaining=' + str(nextTimeRemaining) + ', openedValves=' + str(nextOpenedValves) + ', pressureSoFar=' + str(nextPressureSoFar))
-                    nextValveResult = findPath(
-                        valves=valves, 
-                        pathBetweenValves=pathBetweenValves, 
-                        valveSequence=nextValveSequence, 
-                        unopenedValves=nextUnopenedValves, 
-                        openedValves=nextOpenedValves, 
-                        timeRemaining=nextTimeRemaining, 
-                        pressureSoFar=nextPressureSoFar
-                    )
-                    if valveSequence == originalInput:
-                        print(nextValveResult)
-                    nextSteps.append(nextValveResult)
-                bestDownstream = max(nextSteps, key=lambda s: s[1])
-                return(bestDownstream)
-            else:
+            nextSteps = []
+            for nextValve in potentialNextValves:
                 nextValveSequence = valveSequence.copy()
-                nextPressureSoFar = pressureSoFar + sum([valves[open]['rate'] for open in openedValves])
+                nextValveSequence += [nextValve]
+                nextPressureSoFar = pressureSoFar + sum([valves[openValve]['rate'] * (len(pathBetweenValves[(valve, nextValve)]) + 1) for openValve in openedValves])
                 nextOpenedValves = openedValves.copy()
+                nextOpenedValves += [nextValve]
                 nextUnopenedValves = unopenedValves.copy()
-                nextTimeRemaining = timeRemaining - 1
-                return(findPath(
+                nextUnopenedValves.remove(nextValve)
+                nextTimeRemaining = timeRemaining - len(pathBetweenValves[valve, nextValve]) - 1
+                if valveSequence == originalInput:
+                    print('valveRates=valveRates, start=' + str(nextValveSequence) + ', timeRemaining=' + str(nextTimeRemaining) + ', openedValves=' + str(nextOpenedValves) + ', pressureSoFar=' + str(nextPressureSoFar))
+                nextValveResult = findPath(
                     valves=valves, 
                     pathBetweenValves=pathBetweenValves, 
                     valveSequence=nextValveSequence, 
@@ -136,7 +86,12 @@ def releasePressure(valveRates, start=['AA'], timeRemaining=30, openedValves=[],
                     openedValves=nextOpenedValves, 
                     timeRemaining=nextTimeRemaining, 
                     pressureSoFar=nextPressureSoFar
-                ))
+                )
+                if valveSequence == originalInput:
+                    print(nextValveResult)
+                nextSteps.append(nextValveResult)
+            bestDownstream = max(nextSteps, key=lambda s: s[1])
+            return(bestDownstream)
 
     valves = parseValves(valveRates)
     usefulValves = [valve for valve in valves if valves[valve]['rate'] > 0]
